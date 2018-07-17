0510c6bc69538cadbc94310f2cebd7e112e7029d
Benjamin Siegel <benjaminsiegel@Benjamins-MBP-2.home>
2018-07-17 19:52:18 -0400

    second try

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
-//Create a function that will console.log the user's breakfast when the user presses "Yell Breakfast"
+//CHALLENGE 1: Create a function that dislays on the webpage the user's breakfast in ALL CAPS when the user presses "Yell Breakfast"
 
 
 
 
-//Create a function that will console.log the user's breakfast in ALL CAPS when the user presses "Yell Breakfast"
+//CHALLENGE 2: Create a function that dislays the user's breakfast in all lowercase letters AND a smaller font when the user presses "Whisper Breakfast".
 
 
 
 
-//Create a function that dislays on the webpage the user's breakfast in ALL CAPS when the user presses "Yell Breakfast"
+//CHALLENGE 3: Create a function that dislays on the webpage the user's breakfast with the words "and bacon" when the user presses "Add Bacon." A picture of cripsy delicious bacon should also appear below the text.
 
 
 
 
-//CHALLENGE 1: Create a new button called "Whisper Breakfast." Then create a function that dislays on the webpage the user's breakfast in all lowercase AND a smaller font when the user presses the button.
+//CHALLENGE 4: If the user enters the word "cereal", then display on the webpage "YOU HAD A SUPER AWESOME BREAKFAST!!!!" when the user presses "Judge Breakfast." Code in two other if statements for other possible user responses and a else statement for all other breakfast items.
 
 
 
-//CHALLENGE 2: Add style to the output div so when you display the users breakfast on the webpage it looks GOOD (try playing with font-size, font-weight, text-align, font-family, and backgrounds)
 
 
-
-//CHALLENGE 3: Create a new button called "Add Bacon." Then create a function that dislays on the webpage the user's breakfast with the words "and bacon." A picture of cripsy delicious bacon should also appear below the text
-
-
-
-//CHALLENGE 4: Create a new button called "Judge Breakfast." If the user enters the word "cereal", then display on the webpage "YOU HAD A SUPER AWESOME BREAKFAST!!!!." Code in two other if statements for other possible user responses and a else statement for all other mundane breakfast items.
-
-
-
-//SUPER CHALLENGE 5: Create a new button called "Suggest a Breakfast." Find a food API that fetches recipes and display a recipe on the screen for the user. YOU ARE IN SUPER SAIYAN MODE IF YOU CAN DO THIS!
+//SUPER CHALLENGE 5: Use the recipe api at https://developer.edamam.com/ to fetch a recipe name and image and display it to the user when they press "Suggest a Breakfast". YOU ARE IN SUPER SAIYAN MODE IF YOU CAN DO THIS!
